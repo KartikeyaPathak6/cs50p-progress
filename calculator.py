a4f3635b7be72db740760ed57e3538601b13c17b
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-10 17:19:05 +0530

    update

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
-def add():
-    x= float(input("What's x?"))
-    y= float(input("what's y?"))
-    
-    z= round(x/y,2)
-    
-    print(z)
+def add(x, y):
+    return x + y
+
+def divide(x, y):
+    return round(x / y, 2)
 
 def square(n):
-    n=int(input("Enter a number"))
     return pow(n, 2)
 
-square(add())
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+z = float(input("what's square z? "))
+
+print("Divide",divide(x, y))
+print("Add",add(x,y))
+print("Square",square(z))
