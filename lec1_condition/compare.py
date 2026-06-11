a1f4290328e74b0143eaa5cfa0c8d9129ad1a863
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-11 12:30:22 +0530

    update

---
+++ lec1_condition/compare.py
@@ -0,0 +1,13 @@
+x= int(input("Value for x:"))
+y= int(input("Value for y:"))
+
+#without elif
+
+if x<y:
+    print("x is less than y")
+
+if x>y:
+    print("x is greater than y")
+
+if x==y:
+    print("x is equal to y")
---
+++ lec1_condition/日本語.py
@@ -0,0 +1,9 @@
+x= input("日本語分かりますか？　はい・いや")
+if x=="はい":
+    print("ヤッタ")
+    
+elif x=="いや":
+    print("頑張れ")
+        
+else:
+    print("Error")
