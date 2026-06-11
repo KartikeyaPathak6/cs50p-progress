b9d74711b111216edba1345de1a1f9e98a9cec98
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-11 15:08:20 +0530

    update

---
+++ Lec2_Loops/cat_range.py
@@ -0,0 +1,7 @@
+x= int(input("How much time do you want to meow? :"))
+
+def meow(n):
+    for i in range(n):
+        print("Meow!")
+
+meow(x)
---
+++ Lec2_Loops/cat_while.py
@@ -0,0 +1,5 @@
+i=int(input("How many meow? "))
+
+while i:
+    print("Meow")
+    i=i-1
