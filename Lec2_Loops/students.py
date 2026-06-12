12ba635e37110b9378e1949bd225b070aa1e562e
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-12 12:03:40 +0530

    update

---
+++ Lec2_Loops/students.py
@@ -0,0 +1,9 @@
+name=[]
+x=int(input("No. of students: "))
+for i in range(x):
+    name=(input("Name of the student: "))
+
+for i in range(len(name)):
+    print(i+1, name[i])
+
+#for the moment this code is not working
