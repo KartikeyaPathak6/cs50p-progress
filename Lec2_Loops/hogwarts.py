231a5344f523de1a067c203333eb7d8c8567f1cf
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-12 13:10:46 +0530

    update

---
+++ Lec2_Loops/hogwarts.py
@@ -0,0 +1,14 @@
+students = [
+    {"name": "Hermi", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack"},
+    {"name": "Draco", "house": "Slytherin", "patronus": "None"}
+]
+
+for student in students:
+    print(
+        student["name"],
+        student["house"],
+        student["patronus"],
+        sep=", "
+    )
