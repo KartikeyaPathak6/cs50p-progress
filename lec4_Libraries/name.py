b918f68315e6f8aca4f0f000395757cf8c3964c2
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-25 23:00:57 +0530

    update

--- lec4_Libraries/name.py
+++ lec4_Libraries/name.py
@@ -1,9 +1,7 @@
 import sys
 
-if len(sys.argv)>2:
-    sys.exit("Too many arguments")
-
-elif len(sys.argv)<2:
+if len(sys.argv)<2:
     sys.exit("Too few arguments")
 
-print("Hello, my name is",sys.argv[1])
+for arg in sys.argv[1:-1]:
+    print("Hello, my name is",arg)
