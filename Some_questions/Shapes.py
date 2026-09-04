a58ce1f1bc40b3dd87a0b60e24ce18c33d5eb4b0
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-09-04 19:51:31 +0530

    scratch

---
+++ Some_questions/Shapes.py
@@ -0,0 +1,22 @@
+def main():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter another number: \n"))
+    
+    print("1.Add \n2.Multiply\n3.Divided\n4.Subtract\n")
+
+    while True:
+        try:
+            cal=int(input("Select one: "))
+            match cal:
+                case 1:
+                    print(a+b)
+                case 2:
+                    print(a*b) 
+                case 3:
+                    print(a/b) 
+                case 4:
+                    print(a-b)
+            break
+        except ValueError:
+            print("Value was not an integer")
+main()
